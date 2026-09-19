8898f26d18616f8bed806c37c293b9bd0550783f
NaveenGadugina <gadugina.naveen@s.amity.edu>
2026-09-19 15:58:54 +0530

    added backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     setAnswer("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch("https://skylark-bi-agent-xs94.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
